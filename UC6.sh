ff38e438eaccc3c0266fa069f67fabc311e0672c
keerthika-m <mkeerthika0901@gmail.com>
2023-03-07 18:15:00 +0530

    [karthick] add :using shell script solve to total work days hours

---
+++ UC6.sh
@@ -0,0 +1,43 @@
+#! /bin/bash -x
+
+#constant variable
+is_present_full_time=1
+is_present_part_time=2
+wage_per_hour=20
+max_work_hour=100
+total_working_days=20
+
+#variable
+total_work_hour=0
+day=1
+
+#Loop will execute till both the condition is TRUE
+while (($day <= $total_working_days && $total_work_hour <= $max_work_hour ))
+do
+#variable
+random_check=$((RANDOM%3)) # 0/1/2
+
+#selection
+case $random_check in
+$is_present_full_time)
+emp_hours=8
+echo " Employee is present for full time"
+;;
+$is_present_part_time)
+emp_hours=4
+echo " Employee is present for part time"
+;;
+*)
+emp_hours=0
+echo " Employee is absent"
+;;
+
+esac
+((day++))
+
+total_work_hour=$(( $total_work_hour + $emp_hours ))
+
+done
+
+total_salary=$(( total_work_hour * wage_per_hour))
+echo "Total Salary of an Employee is $total_salary"
